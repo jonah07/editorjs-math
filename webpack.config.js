5305995888c7df8d993c6a626ee4bf27f9490223
Jonah <56933799+jonah07@users.noreply.github.com>
2022-12-10 20:39:35 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,6 @@
 module.exports = {
     entry: './src/index.js',
     mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        query: {
-                            presets: [ '@babel/preset-env' ],
-                        },
-                    },
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                loader: 'file-loader',
-                options: {
-                    name: 'fonts/[name].[ext]',
-                },
-            },
-            {
-                test: /\.(svg)$/,
-                use: [
-                    {
-                        loader: 'raw-loader',
-                    }
-                ]
-            }
-        ]
-    },
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
